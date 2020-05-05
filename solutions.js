d7c865335a8b4792d10647478ba1768feadab7d6
julieg18 <juliannagal18@gmail.com>
2020-05-05 10:43:26 -0500

    complete problem 2

--- solutions.js
+++ solutions.js
@@ -7,3 +7,15 @@ function multiplesOf3And5(num) {
   }
   return multiples.reduce((num1, num2) => num1 + num2);
 }
+
+function fiboEvenSum(n) {
+  const sequence = [1, 2];
+  while (sequence[sequence.length - 1] < n) {
+    const firstNum = sequence[sequence.length - 2];
+    const secondNum = sequence[sequence.length - 1];
+    sequence.push(firstNum + secondNum);
+  }
+  return sequence
+    .filter((num) => num % 2 === 0)
+    .reduce((num1, num2) => num1 + num2);
+}
