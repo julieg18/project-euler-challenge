6d46823b25511338d31c5b9f995e0d70d2e22fd9
julieg18 <juliannagal18@gmail.com>
2020-05-14 08:12:35 -0500

    complete problem 3 and 4

--- solutions.js
+++ solutions.js
@@ -5,7 +5,7 @@ function multiplesOf3And5(num) {
       multiples.push(i);
     }
   }
-  return multiples.reduce((num1, num2) => num1 + num2);
+  return multiples.reduce((maxNum, num2) => maxNum + num2);
 }
 
 function fiboEvenSum(n) {
@@ -17,5 +17,37 @@ function fiboEvenSum(n) {
   }
   return sequence
     .filter((num) => num % 2 === 0)
-    .reduce((num1, num2) => num1 + num2);
+    .reduce((maxNum, num2) => maxNum + num2);
+}
+
+function largestPrimeFactor(number) {
+  let i = 2;
+  let num = number;
+  while (i <= num) {
+    if (num % i === 0) {
+      num /= i;
+    } else {
+      i += 1;
+    }
+  }
+  return i;
+}
+
+function largestPalindromeProduct(n) {
+  const maxNum = '9'.repeat(n) - '';
+  const palindromeProducts = [];
+  const isPalindrome = (num) =>
+    (num + '').split('').reverse().join('') === (num + '').split('').join('');
+  function loopThroughFirstNum(n1, n2) {
+    for (let j = n1; j <= n1 && j > 0; j -= 1) {
+      const product = j * n2;
+      if (isPalindrome(product) && !palindromeProducts.includes(product)) {
+        palindromeProducts.push(product);
+      }
+    }
+  }
+  for (let i = maxNum; i <= maxNum && i > 0; i -= 1) {
+    loopThroughFirstNum(maxNum, i);
+  }
+  return Math.max(...palindromeProducts);
 }
